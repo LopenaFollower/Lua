fca9a7b8c007f1a16e68cec17cf31f2426a60da6
LopenaFollower <115658149+LopenaFollower@users.noreply.github.com>
2022-11-05 00:49:17 +0800

    Update Doors.lua

--- Doors.lua
+++ Doors.lua
@@ -1,20 +1,15 @@
---ver 1
+--ver 2
 --Doors
 local chr = game.Players.LocalPlayer.Character
 local hum = chr and chr:FindFirstChildWhichIsA("Humanoid")
+function goto(obj)
+	while (chr.HumanoidRootPart.Position-obj.Position).magnitude > 5 do
+		chr:TranslateBy((obj.Position-chr.HumanoidRootPart.Position)*0.5)
+	end
+end
 _G.key=false
 while wait(1)do
-	for _,v in pairs(workspace.CurrentRooms:GetDescendants()) do
-		if v.Name=="KeyObtain"and not _G.key then
-			_G.key=true
-			chr:TranslateBy((v.Hitbox.Position-chr.HumanoidRootPart.Position))
-			wait(0.8)
-			chr:TranslateBy((v.Position*1.05-chr.HumanoidRootPart.Position))
-			_G.key=false
-		else
-		if v.Name=="Door"and v:IsA"BasePart"and _G.key then
-			chr:TranslateBy((v.Position*1.05-chr.HumanoidRootPart.Position))
-		end
-		end
+	for _,v in pairs(workspace.CurrentRooms:GetChildren()) do
+		goto(v.Door.Door)
 	end
 end
