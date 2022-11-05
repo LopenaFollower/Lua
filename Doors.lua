75c2cb985729acf276122e3d2ee135b08ec98137
LopenaFollower <115658149+LopenaFollower@users.noreply.github.com>
2022-11-05 22:28:43 +0800

    Update Doors.lua

--- Doors.lua
+++ Doors.lua
@@ -4,12 +4,6 @@ local chr = game.Players.LocalPlayer.Character
 local hum = chr and chr:FindFirstChildWhichIsA("Humanoid")
 function goto(obj)
 	while (chr.HumanoidRootPart.Position-obj.Position).magnitude > 5 do
-		chr:TranslateBy((obj.Position-chr.HumanoidRootPart.Position)*0.5)
-	end
-end
-_G.key=false
-while wait(1)do
-	for _,v in pairs(workspace.CurrentRooms:GetChildren()) do
-		goto(v.Door.Door)
+		chr:TranslateBy((obj.Position-chr.HumanoidRootPart.Position)*0.01)
 	end
 end
