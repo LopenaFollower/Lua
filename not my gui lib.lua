1a1c551b134128945bcf97f469a02fd29db97360
LopenaFollower <115658149+LopenaFollower@users.noreply.github.com>
2023-01-12 21:02:07 +0800

    Update not my gui lib.lua

--- not my gui lib.lua
+++ not my gui lib.lua
@@ -1,8 +1,5 @@
-getgenv().sdasdasdasdadaasdas=true
-wait(1)
 for x = 1,100 do 
 	if game.CoreGui:FindFirstChild("fu8rj82n") then game.CoreGui:FindFirstChild("fu8rj82n"):Destroy() end
-	getgenv().sdasdasdasdadaasdas=false
 end
 
 wait()
